806b49bf9c80379c1cb29797f66faccb55e68cca
Gerhard Baliet <gerhardbaliet@yahoo.de>
2023-06-19 22:57:36 +0200

    contact validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,24 +3,26 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-
   showMailLoader = false;
   showMailSuccess = false;
   showMail = false;
+  nameWarning = false;
+  nameSuccess = false;
+  emailWarning = false;
+  emailSuccess = false;
+  messageWarning = false;
+  messageSuccess = false;
 
-  @ViewChild('myForm')myForm!: ElementRef;
-  @ViewChild('nameField')nameField!: ElementRef;
-  @ViewChild('emailField')emailField!: ElementRef;
-  @ViewChild('messageField')messageField!: ElementRef;
-  @ViewChild('sendBtn')sendBtn!: ElementRef;
+  @ViewChild('myForm') myForm!: ElementRef;
+  @ViewChild('nameField') nameField!: ElementRef;
+  @ViewChild('emailField') emailField!: ElementRef;
+  @ViewChild('messageField') messageField!: ElementRef;
+  @ViewChild('sendBtn') sendBtn!: ElementRef;
 
-
-  
   async sendMail() {
-    
     let nameField = this.nameField.nativeElement;
     let emailField = this.emailField.nativeElement;
     let messageField = this.messageField.nativeElement;
@@ -28,47 +30,71 @@ export class ContactComponent {
 
     nameField.disabled = true;
     emailField.disabled = true;
-    messageField.disabled = true
+    messageField.disabled = true;
     sendBtn.disabled = true;
 
     //loader anzeigen
     this.showMail = true;
     this.showMailLoader = true;
-//animation
-
+    //animation
 
     //senden
-
 
     let fd = new FormData();
     fd.append('name', nameField.value);
     fd.append('email', emailField.value);
     fd.append('message', messageField.value);
-    await fetch('https://gerhard-baliet.developerakademie.net/send_mail/send_mail.php',
+    await fetch(
+      'https://gerhard-baliet.developerakademie.net/send_mail/send_mail.php',
       {
         method: 'POST',
-        body: fd
+        body: fd,
       }
-      )
+    );
 
     //loader ausblenden
     this.showMailLoader = false;
     this.showMailSuccess = true;
     this.myForm.nativeElement.reset();
-    this.showMail = true;
+
     setTimeout(() => {
       this.showMailSuccess = false;
-    this.showMail = false;
-
+      this.showMail = false;
+      console.log(this.showMail);
     }, 500);
 
-
-      //text anzeigen erfolgreich gesendet
+    //text anzeigen erfolgreich gesendet
 
     nameField.disabled = false;
     emailField.disabled = false;
-    messageField.disabled = false
+    messageField.disabled = false;
     sendBtn.disabled = false;
+  }
 
+  checkinputStatus(status: string) {
+     
+    let field = status
+
+    if (this.nameField.nativeElement.value.length === 0 && field == 'name') {
+      this.nameWarning = true;
+      this.nameSuccess = false;
+    } else if (this.nameField.nativeElement.value.length > 0 && field == 'name') {
+      this.nameWarning = false;
+      this.nameSuccess = true;
+    }
+    if (this.emailField.nativeElement.value.length === 0 && field == 'email') {
+      this.emailWarning = true;
+      this.emailSuccess = false;
+    } else if (this.emailField.nativeElement.value.length > 0 && field == 'email') {
+      this.emailWarning = false;
+      this.emailSuccess = true;
+    }
+    if (this.messageField.nativeElement.value.length === 0 && field == 'message') {
+      this.messageWarning = true;
+      this.messageSuccess = false;
+    } else if (this.messageField.nativeElement.value.length > 0 && field == 'message') {
+      this.messageWarning = false;
+      this.messageSuccess = true;
+    }
   }
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -13,8 +13,8 @@ export class ProjectsComponent {
       name: 'El Pollo Loco',
       description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
       image: './assets/img/MockUps/Polloloco.png',
-      url: '',
-      github: '',
+      url: 'https://gerhard-baliet.developerakademie.net/Programming/Projekte/modul_11/index.html',
+      github: 'https://github.com/tzzKr/El-Polo-Loco',
       technologies: ['HTML', 'CSS', 'JavaScript'],
       side: '',
       sidebtn: ''
@@ -24,8 +24,8 @@ export class ProjectsComponent {
       name: 'Join',
       description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
       image: './assets/img/MockUps/Join.png',
-      url: '',
-      github: '',
+      url: 'https://erhard-baliet.developerakademie.net/Programming/Projekte/modul_10/Join-Project/index.html',
+      github: 'https://github.com/tzzKr/Join-Project',
       technologies: ['HTML', 'CSS', 'JavaScript'],
       side: 'flex-direction: row-reverse; text-align: end;',
       sidebtn: 'justify-content: flex-end;'
@@ -34,8 +34,8 @@ export class ProjectsComponent {
       name: 'Pokedex',
       description: 'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
       image: './assets/img/MockUps/Pokedex.png',
-      url: '',
-      github: '',
+      url: 'https://gerhard-baliet.developerakademie.net/Programming/Projekte/modul%209/Pokedex/index.html',
+      github: 'https://github.com/tzzKr/Pokedex',
       technologies: ['HTML', 'CSS', 'JavaScript', 'API'],
       side: '',
       sidebtn: ''
@@ -46,7 +46,7 @@ export class ProjectsComponent {
       description: 'A website for a restaurant',
       image: './assets/img/MockUps/Ringoffire.png',
       url: '',
-      github: '',
+      github: 'https://github.com/tzzKr/RingOfFire',
       technologies: ['HTML', 'SCSS', 'TypeScript', 'Material Design', 'Angular', 'Firebase'],
       side: 'flex-direction: row-reverse; text-align: end; ',
       sidebtn: 'justify-content: flex-end;'
