238f2c94e3cf90f53c6d7d6586f9229ec2691846
Gerhard Baliet <gerhardbaliet@yahoo.de>
2023-06-20 18:03:05 +0200

    linked

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImprintComponent } from './imprint/imprint.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'imprint', component: ImprintComponent },
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
